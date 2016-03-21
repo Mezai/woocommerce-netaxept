8cb68f591c386318ecc0447dfe30efb760e48eac
Johan <johan.tedenmark@hotmail.se>
2016-03-21 23:42:57 +0100

    throw err if env not set

--- lib/Operation.php
+++ lib/Operation.php
@@ -8,6 +8,11 @@ abstract class Operation
         if (!is_array($params)) {
             throw new RequestErrorException("Parameters should be passed as an array");
         }
+
+        if (is_null(Netaxept::getEnvironment())) {
+            throw new EnvironmentNotSetException("Environment is not set, please set it with Netaxept::setEnvironment(TEST) for live and Netaxept::setEnvironment(TEST) for test", 1);
+        }
+
         $request = new Netaxept_HTTP_Request(Netaxept_Environment::getEnvironment() . $path, $params);
 
         $transport = new Netaxept_HTTP_Transport();
